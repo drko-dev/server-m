c5cd2760b2472c5512013f2c5d4d6ffdb7704704
drko <you@sinsalavin.tk>
2018-02-11 23:22:23 +0000

    ssl

---
+++ renew-ssl.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd /opt/letsencrypt/
+./certbot-auto --config /etc/letsencrypt/configs/my-domain.conf certonly
+
+
+if [ $? -ne 0 ]
+ then
+        ERRORLOG=`tail /var/log/letsencrypt/letsencrypt.log`
+        echo -e "The Let's Encrypt cert has not been renewed! \n \n" \
+                 $ERRORLOG
+ else
+        nginx -s reload
+fi
+
+
+exit 0
